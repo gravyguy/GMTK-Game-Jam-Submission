c137d8a94e8437025096d72ea7a041ae28b70629
gravyguy <graycloud@pacbell.net>
2020-07-10 16:21:36 -0700

    Initial Game Manager Upload

---
+++ GMTK Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+using Random = System.Random;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    public int maxTimeBetweenGhosts;
+    public int minTimeBetweenGhosts;
+
+    // Gives player 20 seconds of time to prepare
+    private double timeOfNextGhost = 20f;
+    private double timeBetweenGhosts = 10f;
+
+    // Dictionaries to store possible ingredients for each order and their likelihood (100 means always a component, 0 means never).
+    private Dictionary<string, int> possibleBurgerIngredients;
+    private Dictionary<string, int> possibleSaladIngredients;
+
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        possibleBurgerIngredients.Add("bun", 100);
+        possibleBurgerIngredients.Add("burger", 70);
+        possibleBurgerIngredients.Add("chicken", 20);
+        possibleBurgerIngredients.Add("veggie", 20);
+        possibleBurgerIngredients.Add("cheese", 70);
+        possibleBurgerIngredients.Add("lettuce", 60);
+        possibleBurgerIngredients.Add("tomato", 50);
+        possibleBurgerIngredients.Add("onions", 40);
+        possibleBurgerIngredients.Add("ketchup", 70);
+        possibleBurgerIngredients.Add("mustard", 30);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > timeOfNextGhost)
+        {
+            timeOfNextGhost += timeBetweenGhosts;
+            ghostEvent();
+            // Calculating time before next ghost event:
+            Random r = new Random();
+            timeBetweenGhosts = 5 + r.NextDouble() * 10;
+        }
+    }
+
+    private void ghostEvent()
+    {
+
+    }
+
+    // Make a number of functions that represent different ghost actions, then call on at random
+}
+
+
+
+
+
+
+
+
+
+
---
+++ GMTK Game/Assets/Scripts/Order.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class Order : MonoBehaviour
+{
+    public float timeOfInitialization;
+    public string orderType;
+    public List<string> requiredIngredients;
+
+    public float cutoffForFullPoints;
+    public float cutoffForPartialPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeOfInitialization = Time.timeSinceLevelLoad;
+        determineOrder();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void determineOrder()
+    {
+        Random r = new Random();
+        int rInt = r.Next(0, 4);
+        if (rInt == 0)
+        {
+            orderType = "salad";
+
+        }
+
+    }
+
+    public int CompleteOrder()
+    {
+        int points = 0;
+
+        float timeToMake = Time.time - timeOfInitialization;
+        if (timeToMake < cutoffForFullPoints)
+            points += 50;
+        if (timeToMake < cutoffForPartialPoints)
+            points += 50 * (int)Math.Round(timeToMake / cutoffForFullPoints);
+
+        return points;
+    }
+}
