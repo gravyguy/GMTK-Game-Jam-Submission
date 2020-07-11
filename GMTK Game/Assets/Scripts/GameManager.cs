f2e8fe4a623a381685c05ff71201ee1c1be3d14d
HCEspino <harrison.espino@gmail.com>
2020-07-10 17:29:07 -0700

    Added ghost functions to Game Manager

--- GMTK Game/Assets/Scripts/GameManager.cs
+++ GMTK Game/Assets/Scripts/GameManager.cs
@@ -17,10 +17,14 @@ public class GameManager : MonoBehaviour
     private double timeBetweenGhosts = 10f;
 
     // Dictionaries to store possible ingredients for each order and their likelihood (100 means always a component, 0 means never).
-    private Dictionary<string, int> possibleBurgerIngredients;
-    private Dictionary<string, int> possibleSaladIngredients;
+    public Dictionary<string, int> possibleBurgerIngredients;
+    public Dictionary<string, int> possibleSaladIngredients;
 
+    // Boolean that determines if a ghost event was successfully completed
+    private bool ghostEventSuccess = false;
 
+    // List of possible methods the game manager can call when choosing a ghost event
+    private List<Action> possibleGhostEvents = new List<Action>();
 
     // Start is called before the first frame update
     void Awake()
@@ -30,11 +34,7 @@ public class GameManager : MonoBehaviour
         else if (instance != this)
             Destroy(gameObject);
 
-        // Burger choosing algorithm has to choose one of these
-        possibleBurgerIngredients.Add("burger", 70);
-        possibleBurgerIngredients.Add("chicken", 20);
-        possibleBurgerIngredients.Add("veggie", 10);
-        // Algorithm will chose these at random
+        // Add all possible burger ingredients to the list
         possibleBurgerIngredients.Add("cheese", 70);
         possibleBurgerIngredients.Add("lettuce", 60);
         possibleBurgerIngredients.Add("tomato", 50);
@@ -44,13 +44,21 @@ public class GameManager : MonoBehaviour
         possibleBurgerIngredients.Add("mustard", 30);
         possibleBurgerIngredients.Add("secret sauce", 50);
 
-        // Salad algorithm has no guaranteed ingredients like the burger
+        // Add all possible salad ingredients to the list
         possibleSaladIngredients.Add("chicken", 55);
         possibleSaladIngredients.Add("tomato", 80);
         possibleSaladIngredients.Add("onions", 65);
         possibleSaladIngredients.Add("croutons", 90);
         possibleSaladIngredients.Add("ranch", 100);
         possibleSaladIngredients.Add("secret sauce", 20);
+
+        // Add all possible ghost actions to the list
+        possibleGhostEvents.Add(changeHeat);
+        possibleGhostEvents.Add(moveMeat);
+        possibleGhostEvents.Add(changeLabels);
+        possibleGhostEvents.Add(clogCondiments);
+        possibleGhostEvents.Add(rotVegetable);
+        possibleGhostEvents.Add(reverseOrders);
     }
 
 
@@ -68,12 +76,106 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Chooses a random ghost event to trigger. Repeats until successful
+    /// </summary>
     private void ghostEvent()
     {
 
+        Random r = new Random();
+        ghostEventSuccess = false;
+
+        while (!ghostEventSuccess)
+        {
+            // Chooses a random event in the list
+            possibleGhostEvents[r.Next(0, possibleGhostEvents.Count - 1)]();
+        }
+
+        ghostEventSuccess = true;
     }
 
-    // Make a number of functions that represent different ghost actions, then call on at random
+    /// <summary>
+    /// Randomly changes the heat on one of the grills
+    /// </summary>
+    private void changeHeat()
+    {
+        // Will return false if there is not meat on the grill
+        if (false)
+        {
+            return;
+        }
+
+        //DO STUFF HERE
+
+        ghostEventSuccess = true;
+        return;
+    }
+
+    /// <summary>
+    /// Moves one of the meats on the grill to a different burner
+    /// </summary>
+    private void moveMeat()
+    {
+
+        // Will return false if there is no meat on the grill
+        if (false)
+        {
+            return;
+        }
+
+        // DO STUFF HERE
+
+        ghostEventSuccess = true;
+        return;
+    }
+
+    /// <summary>
+    ///  Switches labels in the condiment section
+    /// </summary>
+    private void changeLabels()
+    {
+        // DO STUFF HERE
+
+        ghostEventSuccess = true;
+        return;
+    }
+
+    /// <summary>
+    ///  Causes the condiment bottle to squeeze unnaturally next time the player picks one up
+    /// </summary>
+    private void clogCondiments()
+    {
+        // DO STUFF HERE
+
+        ghostEventSuccess = true;
+        return;
+    }
+
+    /// <summary>
+    ///  Chooses a random vegetable to be rotten the next time the player picks it up
+    /// </summary>
+    private void rotVegetable()
+    {
+        // DO STUFF HERE
+
+        ghostEventSuccess = true;
+        return;
+    }
+
+    /// <summary>
+    ///  Causes all order tickets to appear upsideown
+    /// </summary>
+    private void reverseOrders()
+    {
+        // Will return false if there is no orders currently to flip
+        if (false)
+        {
+            return;
+        }
+
+        ghostEventSuccess = true;
+        return;
+    }
 }
 
 
