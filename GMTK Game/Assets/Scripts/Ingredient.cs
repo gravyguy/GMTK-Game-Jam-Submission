135ff040535763a96785973c1e9231de968a06a5
gravyguy <graycloud@pacbell.net>
2020-07-11 13:27:17 -0700

    I Honestly Don't Remember What Changer

--- GMTK Game/Assets/Scripts/Ingredient.cs
+++ GMTK Game/Assets/Scripts/Ingredient.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Ingredient : MonoBehaviour
 {
+    public string name;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        gameObject.transform.SetParent(GameObject.Find("IntObjContainer").transform);
     }
 
     // Update is called once per frame
