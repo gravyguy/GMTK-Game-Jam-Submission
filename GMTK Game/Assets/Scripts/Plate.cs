270b3cd70c47676302e2d23af504c815132912d0
gravyguy <graycloud@pacbell.net>
2020-07-11 17:15:24 -0700

    Plate Functionality

---
+++ GMTK Game/Assets/Scripts/Plate.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plate : MonoBehaviour
+{
+    List<GameObject> currentIngredients;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentIngredients = new List<GameObject>();
+    }
+
+    private void OnMouseDown()
+    {
+        if (GameManager.instance.currentItem != null)
+        {
+            if (GameManager.instance.currentItem.tag == "Grilled" || GameManager.instance.currentItem.tag == "Chopped" || GameManager.instance.currentItem.tag == "Condiments")
+            {
+                GameManager.instance.currentItem.transform.position = this.transform.position;
+                GameManager.instance.currentItem.transform.position += new Vector3(0f, .08f * currentIngredients.Count, 0f);
+                GameManager.instance.currentItem.transform.SetParent(this.transform);
+                currentIngredients.Add(GameManager.instance.currentItem);
+                GameManager.instance.currentItem = null;
+            }
+        }
+    }
+
+    public void removeAllIngredients()
+    {
+        for (int i = 0; i < currentIngredients.Count - 1; i++)
+            currentIngredients.RemoveAt(i);
+    }
+
+    public void removeLastIngredient()
+    {
+        currentIngredients.RemoveAt(currentIngredients.Count - 1);
+    }
+}
